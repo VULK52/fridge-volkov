9ecd0e08fe68f5ea23b6c2aa9e71ec9bb676ba26
VULK52 <glebb.volkov@gmail.com>
2025-07-26 12:11:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,49 @@
-const CACHE_NAME = 'my-site-cache-v1';
+const CACHE_NAME = 'naturasnt-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/script.js',
-  '/css/styles.css', // Укажите ваш CSS-файл
-  '/images/icon-192.png'
+  '/css/style.css',
+  '/images/icon-192.png',
+  '/images/icon-512.png',
+  '/images/man.avif',
+  '/images/woman.png',
+  '/catalog.html'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
